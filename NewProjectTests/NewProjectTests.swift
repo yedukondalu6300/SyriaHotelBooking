273d09e0253aec7419b37e44e62c98bcd2d73485
kondalu6300 <GitHub6300@gmail.com>
2025-08-05 15:15:14 +0530

    Initial Commit

---
+++ NewProjectTests/NewProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  NewProjectTests.swift
+//  NewProjectTests
+//
+//  Created by Yarramsetti Yedukondalu on 05/08/25.
+//
+
+import Testing
+@testable import NewProject
+
+struct NewProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
